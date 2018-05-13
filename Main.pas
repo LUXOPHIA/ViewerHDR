0e9ecd9513435d284d2152bf6924cc9336b4e77f
luxidea <contact@luxidea.net>
2018-05-14 04:58:52 +0900

    begin

---
+++ Main.pas
@@ -0,0 +1,80 @@
+﻿unit Main;
+
+interface //#################################################################### ■
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Objects, FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo,
+  LUX.Color.Format.HDR;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Panel1: TPanel;
+    Memo1: TMemo;
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure Image1DragOver(Sender: TObject; const Data: TDragObject; const Point: TPointF; var Operation: TDragOperation);
+    procedure Image1DragDrop(Sender: TObject; const Data: TDragObject; const Point: TPointF);
+  private
+    { private 宣言 }
+  public
+    { public 宣言 }
+    _HDRI :TFileHDR;
+    ///// メソッド
+    procedure ShowData;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation //############################################################### ■
+
+{$R *.fmx}
+
+procedure TForm1.ShowData;
+var
+   K :String;
+begin
+     with Memo1.Lines do
+     begin
+          Clear;
+
+          Add( 'Axis = ' + _HDRI.Axis.ToString );
+          Add( 'Grid.CellsX = ' + _HDRI.Grid.CellsX.ToString );
+          Add( 'Grid.CellsY = ' + _HDRI.Grid.CellsY.ToString );
+
+          Add( '▼ Opetions' );
+
+          for K in _HDRI.Options.Keys do Add( K + ' = ' + _HDRI.Options[ K ] );
+     end;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+     _HDRI := TFileHDR.Create;
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+     _HDRI.DisposeOf;
+end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+procedure TForm1.Image1DragOver(Sender: TObject; const Data: TDragObject; const Point: TPointF; var Operation: TDragOperation);
+begin
+     Operation := TDragOperation.Link;
+end;
+
+procedure TForm1.Image1DragDrop(Sender: TObject; const Data: TDragObject; const Point: TPointF);
+begin
+     _HDRI.LoadFromFile( Data.Files[ 0 ] );
+
+     ShowData;
+
+     _HDRI.Grid.CopyTo( Image1.Bitmap );
+end;
+
+end. //######################################################################### ■
---
+++ ViewerHDR.dpr
@@ -0,0 +1,56 @@
+﻿program ViewerHDR;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Main in 'Main.pas' {Form1},
+  LUX.DN in '_LIBRARY\LUXOPHIA\LUX\LUX.DN.pas',
+  LUX.M2 in '_LIBRARY\LUXOPHIA\LUX\LUX.M2.pas',
+  LUX.M3 in '_LIBRARY\LUXOPHIA\LUX\LUX.M3.pas',
+  LUX.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.M4.pas',
+  LUX in '_LIBRARY\LUXOPHIA\LUX\LUX.pas',
+  LUX.D1 in '_LIBRARY\LUXOPHIA\LUX\LUX.D1.pas',
+  LUX.D2.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.M4.pas',
+  LUX.D2 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.pas',
+  LUX.D2.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D2.V4.pas',
+  LUX.D3.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.M4.pas',
+  LUX.D3 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.pas',
+  LUX.D3.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D3.V4.pas',
+  LUX.D4.M4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.M4.pas',
+  LUX.D4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.pas',
+  LUX.D4.V4 in '_LIBRARY\LUXOPHIA\LUX\LUX.D4.V4.pas',
+  LUX.D5 in '_LIBRARY\LUXOPHIA\LUX\LUX.D5.pas',
+  LUX.Data.Lattice in '_LIBRARY\LUXOPHIA\LUX\Data\LUX.Data.Lattice.pas',
+  LUX.Data.Octree.Atom in '_LIBRARY\LUXOPHIA\LUX\Data\LUX.Data.Octree.Atom.pas',
+  LUX.Data.Octree in '_LIBRARY\LUXOPHIA\LUX\Data\LUX.Data.Octree.pas',
+  LUX.Data.Tree in '_LIBRARY\LUXOPHIA\LUX\Data\LUX.Data.Tree.pas',
+  LUX.Data.Dictionary in '_LIBRARY\LUXOPHIA\LUX\Data\LUX.Data.Dictionary.pas',
+  LUX.Data.Lattice.T2 in '_LIBRARY\LUXOPHIA\LUX\Data\Lattice\LUX.Data.Lattice.T2.pas',
+  LUX.Data.Lattice.T3 in '_LIBRARY\LUXOPHIA\LUX\Data\Lattice\LUX.Data.Lattice.T3.pas',
+  LUX.Data.Lattice.T1 in '_LIBRARY\LUXOPHIA\LUX\Data\Lattice\LUX.Data.Lattice.T1.pas',
+  LUX.Data.Lattice.T1.D1 in '_LIBRARY\LUXOPHIA\LUX\Data\Lattice\T1\LUX.Data.Lattice.T1.D1.pas',
+  LUX.Data.Lattice.T2.D1 in '_LIBRARY\LUXOPHIA\LUX\Data\Lattice\T2\LUX.Data.Lattice.T2.D1.pas',
+  LUX.Data.Lattice.T3.D3 in '_LIBRARY\LUXOPHIA\LUX\Data\Lattice\T3\LUX.Data.Lattice.T3.D3.pas',
+  LUX.FMX.Controls in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Controls.pas',
+  LUX.FMX.Forms in '_LIBRARY\LUXOPHIA\LUX\FMX\LUX.FMX.Forms.pas',
+  LUX.Curve.T1.D2 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T1.D2.pas',
+  LUX.Curve.T1.D3 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T1.D3.pas',
+  LUX.Curve.T2.D1 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T2.D1.pas',
+  LUX.Curve.T2.D2 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T2.D2.pas',
+  LUX.Curve.T2.D3 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T2.D3.pas',
+  LUX.Curve.T1.D1 in '_LIBRARY\LUXOPHIA\LUX\Curve\LUX.Curve.T1.D1.pas',
+  LUX.Color in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\LUX.Color.pas',
+  LUX.Color.Grid.D2 in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\Grid\_FMX\LUX.Color.Grid.D2.pas',
+  LUX.Color.Grid.D3 in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\Grid\_FMX\LUX.Color.Grid.D3.pas',
+  LUX.Color.Grid.D1 in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\Grid\_FMX\LUX.Color.Grid.D1.pas',
+  LUX.Color.Grid.D1.Preset in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\Grid\D1\_FMX\LUX.Color.Grid.D1.Preset.pas',
+  LUX.Color.Grid.D2.Preset in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\Grid\D2\_FMX\LUX.Color.Grid.D2.Preset.pas',
+  LUX.Color.Format.HDR in '_LIBRARY\LUXOPHIA\LUX\LUX.Color\Format\LUX.Color.Format.HDR.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
